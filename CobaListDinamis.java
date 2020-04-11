2fbd2cc03c832cd2fce593cb752e34df5da87dfd
Andi Irham <Andi.irhamm@gmail.com>
2020-04-11 16:09:11 +0700

    [Andi-IM] Revisi File CobaListDinamis.java
    ASW lah hakkon, gabilang-bilang kao ya
    
    Co-Authored-By: Andi Irham <andi.irhamm@gmail.com>

--- CobaListDinamis.java
+++ CobaListDinamis.java
@@ -3,6 +3,8 @@
  * @author Hakkan Syukri 1911082017
  * @author Andi Irham M 1911082006
  */
+
+import java.util.Scanner;
 class NilaiMatkul{                                                   // Deklarasi class NilaiMatkul
     String nim;                                                      // Deklarasi tipe data String pada nim
     String nama;                                                     // Deklarasi tipe data String pada nama
@@ -176,6 +178,104 @@ class List{                                                          // Deklaras
             }
             //pengosongan elemen
             hapus.setNext(null);                                     // elemen hapus dikosongkan
-          }
         }
-  }
+    }
+
+    void delLast(){                                                  // method prosedur delet last
+    Elemen last, before_last;                                        // Deklarasi objek Elemen dengan nama last dan before_last
+        if(first != null){                                           // Jika elemen first tidak kosong
+            if(countElement()==1){                                   // dan jika count elemen bernilai 1
+            //list isi hanya satu
+                delFirst();                                          // hapus elemen pertama
+            }else{ //mencari elemen terakhir                         // jika elemen first kosong
+                last = first;                                        // elemen last diisi elemen first
+                before_last = null;                                  // elemen sebelum last dikosongkan
+
+        while(last.getNext()!= null){                                // selagi last masih ada
+            before_last = last;                                      // elemen sebelum last diisi dengan elemen last 
+            last = last.getNext();                                   // element last diisi dengan elemen last yang baru
+            }
+            //elemen sebelum menjadi elemen terakhir
+        delAfter(before_last);                                       // element before last dikosongkan
+            }
+        }
+    }
+            
+    void printElement(){                                             // method menampilkan elemen
+    Elemen bantu;                                                    // Deklarasi objek Elemen dengan nama bantu
+    int i;                                                           // Deklarasi variabel integer i
+        if(first != null){                                           // jika first tidak kosong 
+            bantu = first;                                           // elemen bantu diisi elemen first
+            i=1;                                                     // i bernilai 1
+        while(bantu != null){                                        // selagi elemen bantu tidak kosong
+            System.out.println("Elemen ke : " +i);                              
+            System.out.println("Nim : " + bantu.getKontainer().getNim());       // tampilkan nim
+            System.out.println("Nama : " + bantu.getKontainer().getNama());     // tampilkan nama
+            System.out.println("Nilai : " + bantu.getKontainer().getNilai());   // tampilkan nilai
+            System.out.println("----------------------");       
+            bantu = bantu.getNext();                                            // buka elemen selanjutnya
+            i=i+1;                                                              // increment i
+            }
+        }else{                                                                  // jika first kosong
+            //list kosong       
+        System.out.println("List Kosong. Data tidak ada");
+        }
+    }
+            
+    void delAll(){                                                   // method menghapus semua elemen
+    int i;                                                           // Deklarasi variabel integer i
+    if(countElement()!=0){                                           // jika count elemen berisi
+        for(i=countElement();i>=1;i--){                              // pengulangan loop mundur
+        //elemen dihapus daru belakang
+        delLast();                                                   // hapus dari belakang
+        }
+    }
+    }
+}
+            
+public class CobaListDinamis {                                       // Main class CobaListDinamis
+    public static void main(String[] args) {                         // Main Method
+    Scanner in = new Scanner(System.in);                             // Deklarasi Scanner
+    List L1 = new List();                                            // Deklarasi List dengan nama L1
+    String katacari;                                                 // Deklarasi String katacari
+    Elemen bantu;                                                    // Deklarasi Elemen bantu
+    int i;                                                           // Deklarasi integer i
+            
+    L1.createList();                                                 // membuat list
+    System.out.println("===Insert===");
+    L1.addFirst("136","Nana","A");                                   // isi nim, nama dan nilai
+    L1.addAfter(L1.getFirst(),"135","Iza","A");                      // menambah setelah "Nana"
+    L1.addLast("137","Rara","A");                                    // menambah elemen terakhir dengan nama "Rara" 
+    L1.addFirst("131","Koko","B");                                   // menambah elemen first dengan nama Koko
+    L1.printElement();                                               // menampilkan semua elemen
+    System.out.println("===================");
+            
+            
+    System.out.print("Masukkan nim yang akan dicari :");
+    katacari = in.nextLine();                                       // mencari nim
+            
+    bantu = L1.getFirst();                                          // bantu diisi dengan getfirst
+    i=1;                                                            // i diisi 1
+        while((bantu != null)&&(!bantu.kontainer.getNim().equals(katacari)) ){  // selama bantu tidak kosong DAN katacari belum ketemu
+        bantu = bantu.getNext();                                                // bantu dicari
+        i=i+1;                                                                  // iterasi increment
+        }
+        if (bantu == null){                                         // jika bantu kosong
+        System.out.println("Data tidak ditemukan");
+        }
+        else{
+            System.out.println("===Hasil Pencarian===");
+            System.out.println(" Elemen ke: "+i);                       // print elemen i
+            System.out.println(" NIM = "+bantu.kontainer.getNim());     // prin NIM elemen i
+            System.out.println(" Nama = "+bantu.kontainer.getNama());   // print nama elemen i
+            System.out.println(" Nilai = "+bantu.kontainer.getNilai()); // print nilai elemen i
+        }
+            
+    System.out.println("\n===Delete===");
+    L1.delFirst();                                                   // delete awal
+    L1.delAfter(L1.getFirst());                                      // delet setelah awal
+    L1.delLast();                                                    // delete terahir
+    L1.printElement();                                               // print kembali semua element
+    System.out.println("===================");
+    }
+}
